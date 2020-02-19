600cfa455694407534b6d3cc4893f31ce9a8088d
Amanda Cheng <acheng6@uncc.edu>
2020-02-18 22:31:15 -0500

    Initial Commit

---
+++ Image/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Image
+//
+//  Created by Amanda Cheng on 2/18/20.
+//  Copyright © 2020 Amanda Cheng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
